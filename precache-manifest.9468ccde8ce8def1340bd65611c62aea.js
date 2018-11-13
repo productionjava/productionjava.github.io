b5a5186e109613fbd98a77c3c43177f7df7a5290
dmitrychaban <dmitrychaban@yahoo.com>
2018-11-13 20:45:12 +0200

    Build

---
+++ precache-manifest.9468ccde8ce8def1340bd65611c62aea.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "7fd3346b85fed95b654333a1c3305faa",
+    "url": "/static/media/book.7fd3346b.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ea9e245ff2a89442c7ec",
+    "url": "/static/js/main.ea9e245f.chunk.js"
+  },
+  {
+    "revision": "a69d00c97b5ff2fb77da",
+    "url": "/static/js/1.a69d00c9.chunk.js"
+  },
+  {
+    "revision": "ea9e245ff2a89442c7ec",
+    "url": "/static/css/main.5c0ed93c.chunk.css"
+  },
+  {
+    "revision": "59d4cede9f378553a3419551d2b22240",
+    "url": "/index.html"
+  }
+];
